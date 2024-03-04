f06099f84d25a91307a731050a0eb82ceebb30c8
CJ Systemas <119875011+CjSystemas@users.noreply.github.com>
2024-03-04 08:21:33 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -61,7 +61,7 @@ def run():
     # Loading the iris dataset
     #https://www.comet.com/site/blog/streamlit-app-for-data-science-projects/
     st.bar_chart(df['Meses'])
-    st.bar_chart(df['Salvador','Curitiba'])
+    st.area_chart(df['Salvador'])
    
 
 if __name__ == "__main__":
