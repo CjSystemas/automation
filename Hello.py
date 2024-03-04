289904f193bbed4ab171575a2d26a1be5d491586
CJ Systemas <119875011+CjSystemas@users.noreply.github.com>
2024-03-04 07:55:40 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -44,7 +44,7 @@ def run():
     #https://drlee.io/building-streamlit-data-web-apps-with-pandas-there-to-tell-the-story-afe03ab71c57
     df = pd.read_excel('gasto_cesta_basica_8_meses.xlsx')
     st.dataframe(df)
-    #st.write(df['Curitiba'].describe())
+    st.write(df['Curitiba'].describe())
     #df_aux = df.describe(include='all')
     #st.write(df_aux)
     #df = df.describe(include='all').fillna("").astype("str")
